af7eb23aaaf968957fa1bad20da0fc855c0f6482
Ulugbek4real <86418633+Ulugbek4real@users.noreply.github.com>
2023-06-17 13:08:08 +0900

    burger menu and main component responsiveness

--- src/components/pages/landing/components/LandingHeader.tsx
+++ src/components/pages/landing/components/LandingHeader.tsx
@@ -1,10 +1,13 @@
 import Link from 'next/link';
 import React from 'react';
 import { BsTelegram } from 'react-icons/bs';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { GrClose } from 'react-icons/gr';
 
 import Logo from '@/assets/logo/logo.svg';
 
 function LandingHeader() {
+  const [isMobile, setIsMobile] = React.useState(false);
   const headerItems = {
     Xizmatlar: '#services',
     Tariflar: '#rates',
@@ -18,40 +21,70 @@ function LandingHeader() {
           <Logo className='h-[50px] w-32 md:w-56' />
         </Link>
         <div className=''>
-          <ul className='apperance-none flex items-center justify-start gap-6 xl:gap-10'>
+          <ul className='apperance-none flex items-center justify-between gap-5 lg:gap-10'>
             {/* Desktop navigation*/}
             {Object.entries(headerItems).map(([key, value]) => (
               <li
                 key={value}
-                className='link link-underline link-underline-black hidden cursor-pointer px-2 text-xl font-semibold text-black md:inline'
+                className='link link-underline link-underline-black hidden cursor-pointer px-2 text-lg font-semibold text-black md:inline'
               >
                 {key}
               </li>
             ))}
 
             <a href="#ta'riflar">
-              <div className='bg-primary flex cursor-pointer items-center justify-end gap-4 rounded-md px-6 py-2 text-white transition-all duration-200 hover:border-none hover:bg-purple-300'>
-                <p>Kirish</p>
+              <div className='bg-primary flex cursor-pointer items-center justify-end gap-4 rounded-md px-4 py-2 text-white transition-all duration-200 hover:border-none hover:bg-purple-300'>
+                <p className='text-sm '>Kirish</p>
               </div>
             </a>
-            <div className='flex  cursor-pointer items-center justify-end gap-4 rounded-md bg-blue-500 px-3 py-2 text-white transition-all duration-200 hover:bg-blue-200 hover:text-blue-500 lg:px-6'>
-              <BsTelegram className='text-2xl' />
+            <div className='hidden cursor-pointer items-center justify-end gap-4 rounded-md bg-blue-500 px-3 py-2 text-white transition-all duration-200 hover:bg-blue-200 hover:text-blue-500 md:flex lg:px-6'>
+              <BsTelegram className='text-xl' />
               <p className='hidden lg:inline'>Kanalga Qo'shilish</p>
+            </div>
+
+            {/* Mobile navigation */}
+            <div
+              onClick={() => setIsMobile(true)}
+              className='-mr-5 flex h-10 w-10 cursor-pointer flex-col items-center justify-center gap-1 md:hidden'
+            >
+              <GiHamburgerMenu className='text-2xl' />
+            </div>
+            <div>
+              <div
+                className={`${
+                  isMobile ? 'right-0' : '-right-full'
+                } fixed top-0 z-50 h-screen w-full bg-white transition-all md:hidden`}
+              >
+                <div className='layout flex items-center justify-between py-6 '>
+                  <Link href='/'>
+                    <Logo className='h-[50px] w-32 md:w-56' />
+                  </Link>
+                  <div
+                    onClick={() => setIsMobile(false)}
+                    className='flex h-10 w-10 cursor-pointer flex-col items-center justify-center gap-1 md:hidden'
+                  >
+                    <GrClose className='text-xl' />
+                  </div>
+                </div>
+                <ul className='layout flex flex-col items-start justify-center gap-5'>
+                  {Object.entries(headerItems).map(([key, value]) => (
+                    <li
+                      key={value}
+                      className='link link-underline link-underline-black cursor-pointer px-2 text-lg font-semibold text-black'
+                    >
+                      {key}
+                    </li>
+                  ))}
+                  <div className='flex cursor-pointer items-center justify-end gap-4 rounded-md bg-blue-500 px-3 py-2 text-white transition-all duration-200 hover:bg-blue-200 hover:text-blue-500'>
+                    <BsTelegram className='text-xl' />
+                    <p className=''>Kanalga Qo'shilish</p>
+                  </div>
+                </ul>
+              </div>
             </div>
           </ul>
         </div>
       </div>
-      {/* Mobile navigation */}
-      {/* <ul className='ml-10 flex md:hidden'>
-        {Object.entries(headerItems).map(([key, value]) => (
-          <li
-            key={value}
-            className='link  link-underline link-underline-black  cursor-pointer px-2 text-xl font-semibold text-black'
-          >
-            {key}
-          </li>
-        ))}
-      </ul> */}
     </nav>
   );
 }
--- src/components/pages/landing/components/LandingMain.tsx
+++ src/components/pages/landing/components/LandingMain.tsx
@@ -6,27 +6,27 @@ import dashboard from '@/assets/dashboard.png';
 function LandingMain() {
   return (
     <div className='bg-gradient relative h-[80vh] w-full'>
-      <div className='layout flex h-full items-center justify-between gap-16'>
-        <div className='flex h-full flex-col items-start justify-between overflow-hidden py-5 md:justify-center lg:w-[500px] xl:w-[690px]'>
-          <div>
-            <h1 className='font-primary leading-12 text-3xl font-semibold tracking-wider  md:text-[55px] md:leading-[60px] xl:text-[63px] xl:leading-[70px]'>
+      <div className='layout flex h-full items-center  justify-between gap-16'>
+        <div className=' flex h-full flex-col justify-between  py-5 md:justify-around 2xl:justify-center 2xl:gap-20 '>
+          <div className=''>
+            <h1 className=' font-primary text-4xl font-semibold tracking-wider md:text-5xl lg:text-6xl'>
               Kam <span className='text-primary'>vaqtda</span>
             </h1>
-            <h1 className='font-primary leading-12 mb-2 text-3xl  font-semibold tracking-wider md:mb-8 md:text-[55px] md:leading-[60px] xl:text-[63px] xl:leading-[70px]'>
+            <h1 className='font-primary text-4xl font-semibold tracking-wider md:mb-6 md:text-5xl lg:text-6xl '>
               katta <span className='text-[#0085FF]'>natijalar</span>!
             </h1>
-            <p className='font-primary xl:text-2 xl max-w-full text-lg leading-6 tracking-wide md:text-2xl md:leading-[38px] md:tracking-wider'>
+            <p className='trackin-wide text-base md:text-xl lg:text-2xl'>
               Ma'lumotlarni daromadga aylantiring.
             </p>
-            <p className='font-primary hidden max-w-full text-lg leading-6 tracking-wide md:text-2xl md:leading-[38px] md:tracking-wider xl:text-2xl'>
+            <p className='trackin-wide hidden text-base md:block md:text-xl lg:text-2xl'>
               Hoziroq o'z muvaffaqiyatingizni boshlang!
             </p>
           </div>
-          <div>
-            <div className='flex items-center justify-start gap-6'>
+          <div className=' '>
+            <div className=' flex items-center justify-start gap-6'>
               <a href="#ta'riflar">
                 {' '}
-                <div className='bg-primary hover:text-primary mt-10 flex cursor-pointer items-center justify-center rounded-lg px-6 py-4 text-2xl text-white shadow-xl transition-all duration-200 hover:bg-purple-300 xl:text-2xl'>
+                <div className=' bg-primary hover:text-primary flex cursor-pointer items-center justify-center  rounded-lg px-3 py-2 text-lg text-white shadow-xl transition-all duration-200 hover:bg-purple-300 md:px-6 md:py-4 md:text-2xl xl:text-2xl'>
                   Ro'yxatdan o'ting
                 </div>
               </a>
@@ -39,7 +39,7 @@ function LandingMain() {
             </p>
           </div>
         </div>
-        <div className='absolute right-0  max-h-[calc(100%-200px)] w-11/12   overflow-hidden rounded-l-lg shadow-xl md:w-2/5 md:min-w-[580px]'>
+        <div className='absolute right-0 max-h-[calc(100%-250px)] w-11/12 overflow-hidden  md:max-h-[calc(100%-200px)] md:w-2/5'>
           <Image
             src={dashboard}
             alt='dashboard'
