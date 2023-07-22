e6afb63e8940c79a5a8f519bc694e06e53b7bf14
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2023-07-22 17:29:24 +0900

    change registration button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
   );
 }
 
-export async function getServerSideProps({ locale }: { locale: any }) {
+export async function getStaticProps({ locale }: { locale: any }) {
   return {
     props: {
       ...(await serverSideTranslations(
