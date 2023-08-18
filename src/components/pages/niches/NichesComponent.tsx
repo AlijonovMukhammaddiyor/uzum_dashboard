13bbf3e29ad57049317c36cc8b4719cd13da82db
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2023-08-19 05:17:34 +0900

    excel

--- src/components/pages/niches/NichesComponent.tsx
+++ src/components/pages/niches/NichesComponent.tsx
@@ -1,13 +1,19 @@
 import { AxiosResponse } from 'axios';
+import * as FileSaver from 'file-saver';
 import { useTranslation } from 'next-i18next';
 import React, { useEffect } from 'react';
+import { FaFileExcel } from 'react-icons/fa';
+import XLSX from 'sheetjs-style';
 
 import API from '@/lib/api';
 import logger from '@/lib/logger';
 
 import { getNichesColDefs } from '@/components/columnDefs';
 import Container from '@/components/layout/Container';
+import Button from '@/components/shared/buttons/Button';
 import Table from '@/components/shared/Table';
+
+import { useContextState } from '@/context/Context';
 
 interface CategoryNichesType {
   category__ancestors: string;
@@ -30,7 +36,7 @@ function NichesComponent() {
   const { t, i18n } = useTranslation('categories');
   const { t: t2 } = useTranslation('tableColumns');
   const [loading, setLoading] = React.useState<boolean>(false);
-
+  const { state } = useContextState();
   const [data, setData] = React.useState<CategoryNichesType[]>([]);
 
   useEffect(() => {
@@ -45,7 +51,7 @@ function NichesComponent() {
         }>
       >(`/category/niches/`)
       .then((res) => {
-        logger(res.data.data, 'category niches');
+        // logger(res.data.data, 'category niches');
         setData(res.data.data);
         setLoading(false);
       })
@@ -56,8 +62,71 @@ function NichesComponent() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  function downloadExcel() {
+    const fileType =
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    const fileExtension = '.xlsx';
+
+    // Filter data to include only the required columns
+    const filteredData = data.map((item: any) => ({
+      category__title_ru:
+        item.category__title_ru?.split('((')[0] ??
+        item.category__title?.split('((')[0],
+      total_orders_amount:
+        Math.round((item.total_orders_amount * 1000) / 1000) * 1000,
+      total_orders: item.total_orders,
+      total_products: item.total_products,
+      total_reviews: item.total_reviews,
+      average_purchase_price: item.average_purchase_price,
+      total_shops: item.total_shops,
+      average_product_rating: item.average_product_rating,
+    }));
+
+    // Convert the filtered data to a sheet
+    const ws = XLSX.utils.json_to_sheet(filteredData);
+
+    // Define custom column headers in Russian
+    const customHeaders = {
+      A1: 'Категория',
+      B1: 'Выручка',
+      C1: 'Заказы',
+      D1: 'Товары',
+      E1: 'Отзывы',
+      F1: 'Средняя цена покупки',
+      G1: 'Магазины',
+      H1: 'Рейтинг товара',
+    };
+
+    // Map custom headers to the sheet
+    for (const key in customHeaders) {
+      const key_: keyof typeof customHeaders = key as any;
+      ws[key_] = { v: customHeaders[key_] };
+    }
+
+    // Create the workbook and save it
+    const wb = { Sheets: { shops: ws }, SheetNames: ['shops'] };
+    const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+    const data2 = new Blob([excelBuffer], { type: fileType });
+    const currentDate = new Date().toISOString().slice(0, 10);
+    FileSaver.saveAs(data2, 'Категории_' + currentDate + fileExtension);
+  }
+
   return (
-    <div className='h-full w-full'>
+    <div className='min-h-full w-full overflow-scroll pb-16'>
+      <Button
+        className='mb-3 flex items-center rounded bg-green-500 px-4 py-2 text-white transition duration-200 ease-in-out hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-50'
+        onClick={downloadExcel}
+        isLoading={loading}
+        spinnerColor='rgb(126 34 206)'
+        disabled={
+          state.user?.tariff === 'free' || state.user?.tariff === 'trial'
+            ? true
+            : false
+        }
+      >
+        <FaFileExcel className='mr-2' />
+        <p>Скачать в Excel</p>
+      </Button>
       <Container
         loading={loading}
         className='h-screen w-full border-none shadow-none'
