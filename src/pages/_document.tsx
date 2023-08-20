2593677043b86ed404f3b9e1d4c1a2fddc90d6e4
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2023-08-20 10:18:01 +0900

    excel

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -107,6 +107,23 @@ export default function Document() {
             }),
           }}
         />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _paq = window._paq = window._paq || [];
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="https://uzanalitikauz.matomo.cloud/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src='//cdn.matomo.cloud/uzanalitikauz.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
