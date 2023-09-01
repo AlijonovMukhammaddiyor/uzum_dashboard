8c843b7584fbaad8199fcc997582f121c62f4099
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2023-09-01 09:47:18 +0900

    add tour to sidebar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -30,6 +30,7 @@ import 'rc-slider/assets/index.css';
 import 'intro.js/introjs.css';
 
 import ArrowLink from '@/components/shared/links/ArrowLink';
+import Tour from '@/components/Tour';
 
 // const Tour = dynamic(() => import('../components/Tour'), { ssr: false });
 import { GOOGLE_CLIENT_ID } from '@/constant/env';
@@ -94,7 +95,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <AuthProvider>
         <main className={ubuntu.className}>
-          {/* {rendered && <Tour />} */}
+          {rendered && <Tour />}
           <Head>
             <meta
               name='viewport'
