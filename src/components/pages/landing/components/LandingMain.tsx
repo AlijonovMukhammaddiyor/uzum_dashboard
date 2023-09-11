8e1164f5dcd336af48a0e015a12262d02f3e1a88
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2023-09-11 13:07:58 +0900

    add youtube video

--- src/components/pages/landing/components/LandingMain.tsx
+++ src/components/pages/landing/components/LandingMain.tsx
@@ -159,7 +159,6 @@ function LandingMain({
             height='92%'
             src='https://www.youtube.com/embed/zsZtVNYyPEc?si=UZBQQLbH7gs_PYt-'
             title='YouTube video player'
-            frameborder='0'
             allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
             allowfullscreen
           ></iframe>
