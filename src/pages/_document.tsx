e5231aa68c2c056f8a8b5f7736bc74e8e1ca65aa
Mukhammaddiyor Alijonov <alijonov@kaist.ac.kr>
2023-09-18 23:28:34 +0900

    remove category trends and shops from trial

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,14 @@ export default function Document() {
           crossOrigin='anonymous'
         />
         <link
+          rel='title'
+          href={
+            i18n.language === 'uz'
+              ? 'UzAnalitika - UZUM market tahlili'
+              : 'UzAnalitika - Аналитика UZUM маркет'
+          }
+        />
+        <link
           href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200&display=swap'
           rel='stylesheet'
         />
@@ -38,17 +46,13 @@ export default function Document() {
           sizes='16x16'
           href='/favicon-16x16.png'
         />
-        <link rel='manifest' href='/site.webmanifest' />
         <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
         <meta name='apple-mobile-web-app-title' content='UzAnalitika' />
         <meta name='application-name' content='UzAnalitika' />
         <meta name='msapplication-TileColor' content='#da532c' />
         <meta name='theme-color' content='#ffffff'></meta>
 
-        <meta
-          name='msapplication-TileImage'
-          content='https://www.uzanalitika.uz/images/mstile-150x150.png'
-        />
+        <meta name='msapplication-TileImage' content='/mstile-150x150.png' />
         <link rel='manifest' href='/site.webmanifest' />
 
         <style>
@@ -115,6 +119,8 @@ export default function Document() {
           src='https://www.googletagmanager.com/gtag/js?id=G-WKZG4TWHPK'
         />
         <script
+          defer
+          async
           dangerouslySetInnerHTML={{
             __html: `
                 window.dataLayer = window.dataLayer || [];
@@ -143,6 +149,8 @@ export default function Document() {
         />
 
         <script
+          defer
+          async
           dangerouslySetInnerHTML={{
             __html: `
               var _paq = window._paq = window._paq || [];
